b8ea96ead77381fc3a659889e14782ccd0fca5b6
Nick Diego Yamane <nick.yamane@openbossa.org>
2013-10-22 17:07:39 -0400

    New changes in nix-env script
    Signed-off-by: Nick Diego Yamane <nick.yamane@openbossa.org>

--- nix-env.sh
+++ nix-env.sh
@@ -17,15 +17,15 @@ if [ -d $NIX ]; then
   export WEBKIT_INSPECTOR_SERVER=127.0.0.1:2222
   export NIX_BUILD_DESKTOP_DIR=$WEBKIT/build/nix-build-desktop
   export NIX_INSTALL_DESKTOP_PREFIX_DIR=$HOME/local
-  
+
   export PATH=$PATH:$NIX_INSTALL_DESKTOP_PREFIX_DIR/bin
   export LD_LIBRARY_PATH=$NIX_INSTALL_DESKTOP_PREFIX_DIR/lib:$LD_LIBRARY_PATH
 
   # configures icecc
   icecc_setup
-  
+
   nix_build_desktop() {
-    JOBS=40
+    JOBS=100
 
     if [ -z $1 ]; then
       CONFIG=release
@@ -37,10 +37,14 @@ if [ -d $NIX ]; then
 
     #[ -d $NIX_BUILD_CONFIG_DIR ] && rm -r $NIX_BUILD_CONFIG_DIR
 
-    PKG_CONFIG_PATH=$NIX_BUILD_DESKTOP_DIR/Dependencies/Root/lib64/pkgconfig \
-    WEBKITOUTPUTDIR=$NIX_BUILD_DESKTOP_DIR \
-    ${NIX}/Tools/Scripts/build-webkit --nix --update-nix --makeargs=-j${JOBS} \
-      --prefix=${HOME}/local --${CONFIG} 2>&1 | tee $WEBKIT/build/build-desktop-${CONFIG}.log
+    # FIXME: Remove PKG_CONFIG_PATH when https://bugs.webkit.org/show_bug.cgi?id=118195 has landed
+    #PKG_CONFIG_PATH=$NIX_BUILD_DESKTOP_DIR/Dependencies/Root/lib64/pkgconfig \
+    CMD="WEBKIT_OUTPUTDIR=$NIX_BUILD_DESKTOP_DIR ${NIX}/Tools/Scripts/build-webkit --nix --makeargs=-j${JOBS} --prefix=${HOME}/local --${CONFIG} --video 2>&1 | tee $WEBKIT/build/build-desktop-${CONFIG}.log"
+
+    echo -------------
+    echo Command: $CMD
+    echo ------------
+    eval "$CMD"
   }
 
   nix_install_desktop() {
@@ -52,14 +56,14 @@ if [ -d $NIX ]; then
       CONFIG=$1
       NIX_BUILD_CONFIG_DIR=${NIX_BUILD_DESKTOP_DIR}/Debug
     fi
-    
+
     pushd $NIX_BUILD_CONFIG_DIR
     make install || echo -ne "\n********* ERROR INSTALL NIX\n"
     popd
   }
 
   nix_update_dependencies() {
-    WEBKITOUTPUTDIR=$NIX_BUILD_DESKTOP_DIR \
+    WEBKIT_OUTPUTDIR=$NIX_BUILD_DESKTOP_DIR \
       ${NIX}/Tools/Scripts/update-webkitnix-libs
   }
 
@@ -102,13 +106,16 @@ drowser_build() {
   DEP_PKGCONFIG_PATH=$NIX_BUILD_DESKTOP_DIR/Dependencies/Root/lib64/pkgconfig
   NIX_PKGCONFIG_PATH=$NIX_INSTALL_DESKTOP_PREFIX_DIR/lib/pkgconfig
 
-  #echo $DEP_PKGCONFIG_PATH --- $NIX_PKGCONFIG_PATH
+  #echo "PKG_CONFIG_PATH=$NIX_PKGCONFIG_PATH:$DEP_PKGCONFIG_PATH cmake $DROWSER && make"
   #return
 
-  pushd $DROWSER_BUILD_CONFIG_DIR 
-  PKG_CONFIG_PATH=$NIX_PKGCONFIG_PATH:$DEP_PKGCONFIG_PATH \
-  cmake $DROWSER && make
-  popd  
+  CMAKE_FLAGS='-DCMAKE_BUILD_TYPE:STRING=Debug'
+
+  cd $DROWSER_BUILD_CONFIG_DIR
+  echo ***Entered $DROWSER_BUILD_CONFIG_DIR
+  echo "PKG_CONFIG_PATH=$NIX_PKGCONFIG_PATH:$DEP_PKGCONFIG_PATH cmake $CMAKE_FLAGS $DROWSER && make"
+  PKG_CONFIG_PATH=$NIX_PKGCONFIG_PATH:$DEP_PKGCONFIG_PATH cmake $CMAKE_FLAGS $DROWSER && make
+  cd -
 }
 
 drowser_run() {
@@ -119,8 +126,10 @@ drowser_run() {
   fi
 
   DROWSER_BUILD_CONFIG_DIR=${DROWSER_BUILD_DESKTOP_DIR_PREFIX}/${CONFIG}
-  LD_LIBRARY_PATH=$HOME/local/lib:$NIX_BUILD_DESKTOP_DIR/Dependencies/Root/lib64 \
-  $DROWSER_BUILD_CONFIG_DIR/src/Browser/drowser
+  LIBRARY_PATH=$HOME/local/lib:$NIX_BUILD_DESKTOP_DIR/Dependencies/Root/lib64
+  echo "running drowser with LD_LIBRARY_PATH=$LIBRARY_PATH ..."
+
+  LD_LIBRARY_PATH=$LIBRARY_PATH $DROWSER_BUILD_CONFIG_DIR/src/Browser/drowser $2
 }
 
 ##################### RPi-related stuff
@@ -139,4 +148,30 @@ alias wk="cd $NIX/Source/WebKit2"
 alias killall-web-process='killall -9 WebProcess'
 
 ## TODO: Layout tests functions
-#./Tools/Scripts/run-webkit-tests --nix --debug --pixel --build-directory=/home/nick/projects/webkit-nix/build/nix-build-desktop --details LayoutTests/compositing/rtl/rtl-absolute-overflow.html
+#export WEBKIT_OUTPUTDIR=/home/nick/projects/webkit-nix/build/nix-build-desktop
+#rm ../layout-test-results/ -rf && ./Tools/Scripts/run-webkit-tests --no-show-results --no-new-test-results --no-sample-on-timeout --results-directory ../layout-test-results --builder-name 'Nix Linux 64-bit Release' --debug --nix --exit-after-n-crashes-or-timeouts 10 --exit-after-n-failures 500 --build-directory=/home/nick/projects/webkit-nix/build/nix-build-desktop
+#strace 2>x.log -- /home/nick/projects/webkit-nix/build/nix-build-desktop/Release/bin/WebKitTestRunner  -
+
+#TODO improve this
+efl_build() {
+    $NIX/Tools/Scripts/build-webkit --efl --no-webkit1 --cmakeargs="-DSHARED_CORE=ON" --makeargs="-j100"
+}
+
+# TODO temp solution / move to use only WEBKIT_OUTPUTDIR
+to_webkit_port() {
+    SRC="$WEBKIT/build/from-port-source-tree/WebKitBuild-$1"
+    DST="$NIX/WebKitBuild"
+
+    if [ -f $DST ]; then
+        echo "Destination dir \"$DST\" already exists and it's not a symbolic link."
+        echo "Aborting.."
+        return 1
+    fi
+
+    [ -L $DST ] && rm $DST
+    ln -sv $SRC $DST
+}
+
+alias tonix='to_webkit_port nix'
+alias toefl='to_webkit_port efl'
+alias togtk='to_webkit_port gtk'
